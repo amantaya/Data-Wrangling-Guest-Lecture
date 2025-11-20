c2510274596b6d2c480ffa37548ddd83d05bebb8
amantaya <andrew.antaya@sdstate.edu>
2025-11-20 12:53:51 -0700

    fix: update file paths in exploring messy data script for consistency and clarity

--- scripts/exploring_messy_data.R
+++ scripts/exploring_messy_data.R
@@ -5,16 +5,21 @@ library("dlookr") # for data exploration
 
 # Load Messy Data ------------------------------------------------------
 
+# Not OK
+messy_data <- read.csv("a/very/specific/file/path/that/will/only/work/on/your/computer/milk_yield.csv")
+
 # OK
-messy_data <- read.csv("data/messy_data.csv")
+messy_data <- read.csv("data/milk_yield.csv")
 
 # Better
-messy_data <- read_csv("data/messy_data.csv")
+messy_data <- read_csv(
+  file.path("data", "milk_yield.csv")
+)
 
 # Best
 messy_data <- readr::read_csv(
-    here::here("data", "messy_data.csv")
-    )
+  here::here("data", "milk_yield.csv")
+)
 
 # Explore Messy Data ---------------------------------------------------
 
